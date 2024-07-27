5d42f7163bcd4820b12c440a396d7f02de9223b4
bot-of-flesh <le25mai5@gmail.com>
2024-07-27 22:53:32 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -127,24 +127,10 @@ function App() {
     setBox([]);
         const regions = result.outputs[0].data.regions;
         regions.forEach(region => {
-            // Accessing and rounding the bounding box values
-
             const boundingBox = region.region_info.bounding_box;
-            const topRow = boundingBox.top_row.toFixed(3);
-            const leftCol = boundingBox.left_col.toFixed(3);
-            const bottomRow = boundingBox.bottom_row.toFixed(3);
-            const rightCol = boundingBox.right_col.toFixed(3);
             displayFaceBox(calculateFaceLocation(boundingBox));
-            region.data.concepts.forEach(concept => {
-                // Accessing and rounding the concept value
-                const name = concept.name;
-                const value = concept.value.toFixed(4);
-                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
-                return user;
             });
-        });
-
-    })
+        })
     .catch(error => console.log('error', error));
   }
 
