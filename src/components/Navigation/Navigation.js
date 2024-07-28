5f838009e74970aaa8a7ecd5418bd9f0df9bd6bd
bot-of-flesh <le25mai5@gmail.com>
2024-07-28 23:01:10 +0900

    add instruction

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,16 +3,19 @@ import React from "react";
 const Navigation = ({OnRouteChange, isSignIn}) =>{
         if (isSignIn){
             return(
-                <nav style={{display:'flex', justifyContent: 'flex-end'}}>
-                    <p className="f3 link dim black underline pa3 pointer" onClick={() => OnRouteChange('signout')}>サインアウト</p>
-                </nav>
+                    <nav style={{display: 'flex', justifyContent: 'flex-end'}} className="instruction">
+                        <p className="f3 link dim black underline pa3 pointer" onClick={() => OnRouteChange('signout')}>サインアウト</p>
+                    </nav>
             );
         } else {
             return(
-                <nav style={{display:'flex', justifyContent: 'flex-end'}}>
-                    <p className="f3 link dim black underline pa3 pointer" onClick={() => OnRouteChange('signin')}>サインイン</p>
-                    <p className="f3 link dim black underline pa3 pointer" onClick={() => OnRouteChange('register')}>登録</p>
-                </nav>
+                <div className="message" >
+                    <p className="f3  black pa3">未登録の方は登録の後でサインインを、登録済みの方はそのままサインインをしてください。</p>
+                    <nav>
+                        <p className="f3 link dim black underline pa3 pointer" onClick={() => OnRouteChange('signin')}>サインイン</p>
+                        <p className="f3 link dim black underline pa3 pointer" onClick={() => OnRouteChange('register')}>登録</p>
+                    </nav>
+                </div>
             );
         }
 }
