30372feadf9aae7abd712dc60a0c55530b5a18af
bot-of-flesh <le25mai5@gmail.com>
2025-02-07 17:48:28 +0900

    文章の変更

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) =>{
     return(
         <div>
             <p className="f3">
-                {`スマートブレインが画像の中の顔を認識します。ぜひお試しください！`}
+                {`スマートブレインが画像の中の顔を認識します。URLを入力してください。`}
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
